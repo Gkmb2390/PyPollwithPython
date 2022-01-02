07335e3e79c9b3cb832aaa63f22d9aad5792e84e
Kevin Blum <gkmb23@Gmail.com>
2022-01-02 11:06:27 -0600

    completed 3.5.3

--- Election_Analysis/poll.py
+++ Election_Analysis/poll.py
@@ -9,8 +9,10 @@ file_to_save = os.path.join("analysis", "election_analysis.txt")
 #Accumulators i.e. counters
 #1) Total Vote Counter
 total_votes = 0
+#declare empty candidate list 
 candidate_options = []
-
+#declare empty candidate dictionary 
+candidate_votes = {}
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
 
@@ -23,16 +25,23 @@ with open(file_to_load) as election_data:
     
     #Print Each Row in the CSV File
     for row in file_reader:
-       #print(row)
-       #2 Add to Total Vote Count 
-       total_votes += 1
-       #print the candidate names from each row
-       candidate_name = row[2]  
-       # if the candidate does not match any existing candidate
-       if candidate_name not in candidate_options:
-       # add the candidate name to the candidate list.
-        candidate_options.append(candidate_name)
+        #print(row)
+        #2 Add to Total Vote Count 
+        total_votes += 1
+        #print the candidate names from each row
+        candidate_name = row[2]  
+        # if the candidate does not match any existing candidate
+        if candidate_name not in candidate_options:
+        # add the candidate name to the candidate list.
+            candidate_options.append(candidate_name)
+            # Begin Tracking Candidate vote count
+            candidate_votes[candidate_name] = 0
+
+        #increase candidate votes by 1
+        candidate_votes[candidate_name] +=1
 #3 Print Total Votes
-print(total_votes)
+#print(total_votes)
 #4 print candidate list
-print(candidate_options)
+#print(candidate_options)
+#5 print candidate vote dictionary
+print(candidate_votes)
